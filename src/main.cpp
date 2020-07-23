a6efe899c9cb8ca11cfd0890780a78467ad314f3
Glebanister <glebmar2001@gmail.com>
2020-07-23 11:07:01 +0300

    Init git

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+namespace peach
+{ 
+class Token
+{
+};
+} // namespace peach
+
+/*
+
+func foo(a, b, c)
+    let res = 0
+    for (let i = 0; i < 10; i = i + 1)
+        res = res + a + b + i
+    res = res + c
+    return res
+
+let res = foo(1, 2, 3)
+print(res)
+
+*/
