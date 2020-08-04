e5b25afa894a9fe6a94da990eedacc65ac0ef6f9
Glebanister <glebmar2001@gmail.com>
2020-08-04 11:46:13 +0300

    Implement Node and CharTransition

---
+++ include/Token.hpp
@@ -0,0 +1,182 @@
+#ifndef TOKEN_HPP
+#define TOKEN_HPP
+
+#include <string>
+
+namespace peach
+{
+namespace token
+{
+enum class tokenCategory
+{
+    /*
+    Token is undefined
+    */
+    UNDEFINED,
+
+    /*
+    Token is a name,
+    Starts with a latin letter,
+    May contain:
+        - contains digits
+        - latin letters
+        - underscores
+    Examples:
+        - 'VariableName1'
+        - 'variable_name_2'
+    */
+    NAME,
+
+    /*
+    Token is a value - predefined, or user-defined,
+    Examples:
+        - '123'                for std::int32_t
+        - '"this is a string"' for std::string
+    */
+    VALUE,
+
+    /*
+    Token is an operator - unary or binary,
+    Descending priority:
+        - 'not', '!' logical negation
+        - 'and', '&' logical and
+        - 'or', '|'  logical or
+        - '*'        multiplication
+        - '/'        division
+        - '%'        module
+        - '+'        addition
+        - '-'        substraction
+    */
+    OPERATOR,
+
+    /*
+    Token states for assignment,
+    Only name token may be on the left,
+    Any expression could be on the right.
+        - '='  assign
+        - 'X=' X and assign
+    */
+    ASSIGNMENT,
+
+    /*
+    Token is 'if' in conditional chain
+    */
+    COND_IF,
+
+    /*
+    Token is 'elif' in conditional chain
+    */
+    COND_ELIF,
+
+    /*
+    Token is 'else' in conditional chain
+    */
+    COND_ELSE,
+
+    /*
+    Token is 'for' loop
+    */
+    LOOP_FOR,
+
+    /*
+    Token is 'while' loop
+    */
+    LOOP_WHILE,
+
+    /*
+    Token is a opening round bracket
+    */
+    BRACKET_OPEN,
+
+    /*
+    Token is a closing round bracket
+    */
+    BRACKET_CLOSE,
+
+    /*
+    Token is an endline character
+    */
+    SEP_ENDL,
+
+    /*
+    Token is a tabulation character
+    */
+    SEP_TAB,
+
+    /*
+    Token is a space character
+    */
+    SEP_SPACE
+};
+
+class Token
+{
+public:
+    Token(tokenCategory category)
+        : category_(std::move(category)) {}
+
+    tokenCategory getCategory() const noexcept
+    {
+        return category_;
+    }
+
+    virtual std::string_view getToken() const = 0;
+
+private:
+    tokenCategory category_;
+};
+
+template <tokenCategory Category>
+class StringOwnerToken
+{
+public:
+    UndefinedToken(const std::string &token)
+        : Token(Category),
+          token_(token)
+    {
+    }
+
+    UndefinedToken(std::string &&token)
+        : Token(Category),
+          token_(std::move(token))
+    {
+    }
+
+    std::string_view getToken() const override
+    {
+        return token_;
+    }
+
+private:
+    std::string token_;
+};
+
+using UndefinedToken = StringOwnerToken<tokenCategory::UNDEFINED>;
+using NameToken = StringOwnerToken<tokenCategory::NAME>;
+
+class NameToken : public Token
+{
+    NameToken(const std::string &token)
+        : Token(tokenCategory::NAME),
+          token_(token)
+    {
+    }
+
+    NameToken(std::string &&token)
+        : Token(tokenCategory::NAME),
+          token_(std::move(token))
+    {
+    }
+
+    std::string_view getToken() const override
+    {
+        return token_;
+    }
+
+private:
+    std::string token_;
+};
+} // namespace token
+} // namespace peach
+
+#endif // TOKEN_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,100 @@
-namespace peach
-{ 
-class Token
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <unordered_map>
+#include <vector>
+
+#include "Token.hpp"
+
+using namespace peach;
+
+class CharTransition
 {
+public:
+    virtual bool isActive(char) = 0;
+    virtual ~CharTransition() = default;
 };
-} // namespace peach
+
+template <char Begin, char End>
+class RangeCharTransition : public CharTransition
+{
+public:
+    bool isActive(char c) override
+    {
+        return Begin <= c && c <= End;
+    }
+};
+
+template <char Char>
+using SingleCharTransition = RangeCharTransition<Char, Char>;
+
+template <char... Chars>
+class SetCharTransition : public CharTransition
+{
+public:
+    bool isActive(char c) override
+    {
+        bool result = false;
+        ((result |= Chars == c), ...);
+        return result;
+    }
+};
+
+class Node
+{
+public:
+    template <typename Transition, typename... TransitionArgs>
+    void addTransition(std::shared_ptr<Node> nextNode, TransitionArgs &&... args)
+    {
+        auto newTransition = std::make_unique<Transition>(std::forward(args)...);
+        transitions_.emplace_back(std::move(newTransition), std::move(nextNode));
+    }
+
+    std::shared_ptr<Node> getNextNode(char c)
+    {
+        for (auto &&[transition, node] : transitions_)
+        {
+            if (transition->isActive(c))
+            {
+                return node;
+            }
+        }
+        return nullptr;
+    }
+
+private:
+    std::vector<std::pair<std::unique_ptr<CharTransition>, std::shared_ptr<Node>>> transitions_;
+};
+
+template <typename... Args>
+inline std::shared_ptr<Node> makeNode(Args &&... args)
+{
+    return std::make_shared<Node>(std::forward(args)...);
+}
+
+int main()
+{
+    auto node1 = makeNode();
+    auto node2 = makeNode();
+    node1->addTransition<SetCharTransition<'a'>>(node2);
+    std::cout << node1->getNextNode('b') << std::endl;
+}
 
 /*
 
-func foo(a, b, c)
-    let res = 0
-    for (let i = 0; i < 10; i = i + 1)
-        res = res + a + b + i
+func foo(a, b, c):
+    let res = 4
+    let i = 0
+    while (res != 0):
+        i = i + 1
+        res = res - 1
+        if i % 2 == 0:
+            res = res + a + b + i
+        elif i % 2 == 1:
+            res = 0
+        else:
+            res = res + c - i
     res = res + c
     return res
-
-let res = foo(1, 2, 3)
-print(res)
-
 */
