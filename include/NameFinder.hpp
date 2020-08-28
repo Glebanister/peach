27192b0f902dbc665d3a9d6a56443fe92501a3e4
Glebanister <glebmar2001@gmail.com>
2020-08-28 12:38:17 +0300

    Implement name and operator finders

--- include/NameFinder.hpp
+++ include/NameFinder.hpp
@@ -6,8 +6,8 @@ namespace peach
 {
 namespace fsm
 {
-// Name here - pattern that starts with latin letter or underscore, contains latin letters, underscores and digits
-class NameFinder : protected FiniteStateMachine
+// Finite state machine, finds tokens with category tokenCategory::NAME
+class NameFinder : public FiniteStateMachine
 {
 public:
     NameFinder()
@@ -15,25 +15,6 @@ public:
         auto firstNode = getRoot()->addTransitionToNewNode<transition::LatinUnderscoreTransition>(token::tokenCategory::UNDEFINED);
         firstNode->addTransition<transition::LatinUnderscoreDigitTransition>(firstNode);
         firstNode->addTransitionToNewNode<transition::TransitionNegation<transition::LatinUnderscoreDigitTransition>>(token::tokenCategory::NAME);
-        getRoot()->addTransition<transition::TransitionNegation<transition::LatinUnderscoreTransition>>(getRoot());
-    }
-
-    std::vector<std::unique_ptr<token::Token>> tokenizeText(const std::string &text,
-                                                     const std::unordered_map<std::string, token::tokenCategory> &reservedNames = {})
-    {
-        auto tokens = FiniteStateMachine::tokenizeText(text);
-        static_assert(static_cast<int>(token::tokenCategory::UNDEFINED) == 0); // for unordered map
-        for (auto &token : tokens)
-        {
-            auto string = token->getTokenString();
-            auto it = reservedNames.find(string);
-            if (it == reservedNames.cend())
-            {
-                continue;
-            }
-            token->setCategory(it->second);
-        }
-        return tokens;
     }
 };
 } // namespace fsm
---
+++ include/OperatorFinder.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <array>
+#include <stdexcept>
+
+#include "FiniteStateMachine.hpp"
+
+namespace peach
+{
+namespace fsm
+{
+// Finite state machine, finds tokens with category tokenCategory::OPERATOR
+class OperatorFinder : public FiniteStateMachine
+{
+public:
+    OperatorFinder(const std::vector<std::string> &operators)
+    {
+        std::for_each(operators.begin(), operators.end(), [&](const auto &pat) {
+            addOperatorPattern(pat);
+        });
+    }
+
+    // Adds pattern to finite state machine.
+    // Should not contain latin letters, digits, separators or underscores.
+    void addOperatorPattern(const std::string &pattern)
+    {
+        auto curNode = getRoot();
+        std::array<std::unique_ptr<transition::CharTransition>, 2> badTransitions =
+            {std::make_unique<transition::LatinUnderscoreDigitTransition>(),
+             std::make_unique<transition::SetCharTransition>(std::vector<char>{' ', '\n', '\t'})};
+        for (char c : pattern)
+        {
+            for (auto &tr : badTransitions)
+            {
+                if (tr->isActive(c))
+                {
+                    throw std::invalid_argument("character " + std::to_string(c) + " is not allowed in operators");
+                }
+            }
+            curNode = curNode->addTransitionToNewNode<transition::SingleCharTransition>(token::tokenCategory::UNDEFINED, c);
+        }
+        curNode->addTransitionToNewNode<transition::TrueTransition>(token::tokenCategory::OPERATOR);
+    }
+};
+} // namespace fsm
+} // namespace peach
