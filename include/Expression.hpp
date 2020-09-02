a7ace548c9601fd0f17c46fac340b6adf5690684
Glebanister <glebmar2001@gmail.com>
2020-09-02 09:57:57 +0300

    Implement FunctionCall

--- include/Expression.hpp
+++ include/Expression.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <array>
+#include <functional>
+#include <memory>
 #include <string>
+#include <tuple>
+#include <type_traits>
 #include <unordered_map>
 
 namespace peach
@@ -8,21 +13,99 @@ namespace peach
 namespace expression
 {
 // Variables type
-using VType = std::int32_t;
+using VType = std::int32_t; // TODO: wtf is this #1
 
 // Scope is the current state of visible variables
-using Scope = std::unordered_map<std::string, VType>; // TODO: wtf is this
+using Scope = std::unordered_map<std::string, VType>; // TODO: wtf is this #2
 
 struct RvalueAndScope
 {
-    VType rvalue;
+    VType value;
     Scope scope;
 };
 
 class Expression
 {
 public:
-    virtual RvalueAndScope eval() = 0;
+    virtual RvalueAndScope eval(Scope) = 0;
+    virtual ~Expression() = default;
+};
+
+namespace details
+{
+// Function return type correct iff it returns VType
+template <typename Function, typename... Args>
+struct IsFunctionRetTypeCorrect
+{
+    constexpr static bool value = std::is_same_v<std::invoke_result_t<Function, Args...>, VType>;
+};
+
+// Generates tuple of T of length N
+template <typename T, std::size_t N, typename... Tail>
+struct TupleGenerator
+{
+    using type = typename TupleGenerator<T, N - 1, T, Tail...>::type;
+};
+
+template <typename T, typename... Tail>
+struct TupleGenerator<T, 0, Tail...>
+{
+    using type = std::tuple<Tail...>;
+};
+
+template <typename T, std::size_t N>
+using TupleGenerator_t = typename TupleGenerator<T, N>::type;
+} // namespace details
+
+template <std::size_t Arity>
+class FunctionCall : public Expression
+{
+public:
+    using FunctionType = std::function<VType(details::TupleGenerator_t<VType, Arity>)>;
+    constexpr static std::size_t arity = Arity;
+
+    FunctionCall(std::array<std::unique_ptr<Expression>, Arity> &&expressions,
+                 FunctionType f)
+        : expressions_(std::move(expressions)), f_(std::move(f)) {}
+
+    RvalueAndScope eval(Scope scope) override
+    {
+        Scope currentScope = scope;
+        details::TupleGenerator_t<VType, Arity> callArgs;
+        std::size_t argumentId = 0;
+        auto processSingleArgument = [&](VType &argument) {
+            auto evalResult = expressions_[argumentId++]->eval(currentScope);
+            currentScope = evalResult.scope;
+            argument = evalResult.value;
+        };
+        std::apply([&](auto &&... args) {
+            (processSingleArgument(args), ...);
+        },
+                   callArgs);
+        return {f_(callArgs), currentScope};
+    }
+
+private:
+    std::array<std::unique_ptr<Expression>, Arity> expressions_;
+    FunctionType f_;
+};
+
+using UnaryOperator = FunctionCall<1>;
+using BinaryOperator = FunctionCall<2>;
+
+class VTypeValue : public Expression
+{
+public:
+    VTypeValue(VType value)
+        : value_(std::move(value)) {}
+
+    RvalueAndScope eval(Scope scope) override
+    {
+        return {value_, scope};
+    }
+
+private:
+    VType value_;
 };
 } // namespace expression
 } // namespace peach
--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,12 @@
 #include <unordered_map>
 #include <vector>
 
+#include "Expression.hpp"
 #include "FsmCollection.hpp"
 #include "NameFinder.hpp"
 #include "NumberFinder.hpp"
 #include "OperatorFinder.hpp"
 #include "SingleCharFinder.hpp"
-
 #include "Transition.hpp"
 
 int main()
@@ -18,92 +18,84 @@ int main()
     using peach::token::tokenCategory;
     using peach::token::tokenCategory_t;
     using namespace peach::transition;
-    auto finder = peach::fsm::FsmCollection();
-    finder
-        .buildAppendFsm<peach::fsm::NameFinder>()                                     //
-        .buildAppendFsm<peach::fsm::NumberFinder>(tokenCategory::VALUE_FLOATING, '.') //
-        .buildAppendFsm<peach::fsm::NumberFinder>(tokenCategory::VALUE_INT)           //
-        .buildAppendFsm<peach::fsm::OperatorFinder>(
-            std::vector<std::pair<std::string, tokenCategory_t>>{
-                {"!", tokenCategory::OPERATOR_UN},
-                {"&", tokenCategory::OPERATOR_BI},
-                {"|", tokenCategory::OPERATOR_BI},
-                {"*", tokenCategory::OPERATOR_BI},
-                {"/", tokenCategory::OPERATOR_BI},
-                {"%", tokenCategory::OPERATOR_BI},
-                {"+", tokenCategory::OPERATOR_BI},
-                {"-", tokenCategory::OPERATOR_BI},
-                {"=", tokenCategory::ASSIGNMENT},
-                {"==", tokenCategory::OPERATOR_BI},
-                {"&=", tokenCategory::ASSIGNMENT},
-                {"|=", tokenCategory::ASSIGNMENT},
-                {"*=", tokenCategory::ASSIGNMENT},
-                {"/=", tokenCategory::ASSIGNMENT},
-                {"%=", tokenCategory::ASSIGNMENT},
-                {"+=", tokenCategory::ASSIGNMENT},
-                {"-=", tokenCategory::ASSIGNMENT},
-            }) //
-        .buildAppendFsm<peach::fsm::SingleCharFinder>(
-            std::vector<std::pair<char, tokenCategory_t>>{
-                {'\n', tokenCategory::SEP_ENDL},
-                {' ', tokenCategory::SEP_SPACE},
-                {'\t', tokenCategory::SEP_TAB},
-                {'(', tokenCategory::BRACKET_OPEN},
-                {')', tokenCategory::BRACKET_CLOSE},
-                {':', tokenCategory::COLON},
-                {';', tokenCategory::SEMICOLON},
-            }) //
-        ;
+    // auto finder = peach::fsm::FsmCollection();
+    // finder
+    //     .buildAppendFsm<peach::fsm::NameFinder>()                                     //
+    //     .buildAppendFsm<peach::fsm::NumberFinder>(tokenCategory::VALUE_FLOATING, '.') //
+    //     .buildAppendFsm<peach::fsm::NumberFinder>(tokenCategory::VALUE_INT)           //
+    //     .buildAppendFsm<peach::fsm::OperatorFinder>(
+    //         std::vector<std::pair<std::string, tokenCategory_t>>{
+    //             {"!", tokenCategory::OPERATOR_UN},
+    //             {"&", tokenCategory::OPERATOR_BI},
+    //             {"|", tokenCategory::OPERATOR_BI},
+    //             {"*", tokenCategory::OPERATOR_BI},
+    //             {"/", tokenCategory::OPERATOR_BI},
+    //             {"%", tokenCategory::OPERATOR_BI},
+    //             {"+", tokenCategory::OPERATOR_BI},
+    //             {"-", tokenCategory::OPERATOR_BI},
+    //             {"=", tokenCategory::ASSIGNMENT},
+    //             {"==", tokenCategory::OPERATOR_BI},
+    //             {"&=", tokenCategory::ASSIGNMENT},
+    //             {"|=", tokenCategory::ASSIGNMENT},
+    //             {"*=", tokenCategory::ASSIGNMENT},
+    //             {"/=", tokenCategory::ASSIGNMENT},
+    //             {"%=", tokenCategory::ASSIGNMENT},
+    //             {"+=", tokenCategory::ASSIGNMENT},
+    //             {"-=", tokenCategory::ASSIGNMENT},
+    //         }) //
+    //     .buildAppendFsm<peach::fsm::SingleCharFinder>(
+    //         std::vector<std::pair<char, tokenCategory_t>>{
+    //             {'\n', tokenCategory::SEP_ENDL},
+    //             {' ', tokenCategory::SEP_SPACE},
+    //             {'\t', tokenCategory::SEP_TAB},
+    //             {'(', tokenCategory::BRACKET_OPEN},
+    //             {')', tokenCategory::BRACKET_CLOSE},
+    //             {':', tokenCategory::COLON},
+    //             {';', tokenCategory::SEMICOLON},
+    //         }) //
+    //     ;
 
-    auto text = "whi = 1 -4\n"
-                "whilel=4123123\n"
-                "i = 0\n"
-                "while (res != 0)\n"
-                "    i = i + 1\n"
-                "    res = res - 1\n"
-                "    if i % 2 == 0:\n"
-                "        res = res + a + b + i\n"
-                "    elif i % 2 == 1:\n"
-                "        res_2 = 0\n"
-                "    else:\n"
-                "        res = res + c - i\n"
-                "res += c\n";
+    // auto text = "whi = 1 -4\n"
+    //             "whilel=4123123\n"
+    //             "i = 0\n"
+    //             "while (res != 0)\n"
+    //             "    i = i + 1\n"
+    //             "    res = res - 1\n"
+    //             "    if i % 2 == 0:\n"
+    //             "        res = res + a + b + i\n"
+    //             "    elif i % 2 == 1:\n"
+    //             "        res_2 = 0\n"
+    //             "    else:\n"
+    //             "        res = res + c - i\n"
+    //             "res += c\n";
 
-    for (const auto &token : finder.tokenizeText(text, {
-                                                           {"if", tokenCategory::COND_IF},
-                                                           {"elif", tokenCategory::COND_ELIF},
-                                                           {"else", tokenCategory::COND_ELSE},
-                                                           {"while", tokenCategory::LOOP_WHILE},
-                                                       }))
-    {
-        if (token->getCategory() != peach::token::tokenCategory::UNDEFINED)
-        {
-            std::cout << token->getTokenString();
-            // std::string str = token->getTokenString();
-            // if (str == "\n")
-            // {
-            //     str = "\\n";
-            // }
-            // std::cout << token->getPosition() << ' ' << '\'' << str << '\'' << ' ';
-            // peach::token::printCategory(token->getCategory());
-            // std::cout << std::endl;
-        }
-    }
+    // for (const auto &token : finder.tokenizeText(text, {
+    //                                                        {"if", tokenCategory::COND_IF},
+    //                                                        {"elif", tokenCategory::COND_ELIF},
+    //                                                        {"else", tokenCategory::COND_ELSE},
+    //                                                        {"while", tokenCategory::LOOP_WHILE},
+    //                                                    }))
+    // {
+    //     if (token->getCategory() != peach::token::tokenCategory::UNDEFINED)
+    //     {
+    //         std::cout << token->getTokenString();
+    //         // std::string str = token->getTokenString();
+    //         // if (str == "\n")
+    //         // {
+    //         //     str = "\\n";
+    //         // }
+    //         // std::cout << token->getPosition() << ' ' << '\'' << str << '\'' << ' ';
+    //         // peach::token::printCategory(token->getCategory());
+    //         // std::cout << std::endl;
+    //     }
+    // }
+
+    using namespace peach;
+
+    expression::Scope emptyScope;
+    expression::FunctionCall<1> neg({std::make_unique<expression::VTypeValue>(10)},
+                                    [](std::tuple<peach::expression::VType> args) { return -std::get<0>(args); });
+    std::cout << neg.eval(emptyScope).value << std::endl;
 
     return 0;
 }
-
-/*
-res = 4
-i = 0
-while (res != 0)
-    i = i + 1
-    res = res - 1
-    if i % 2 == 0:
-        res = res + a + b + i
-    elif i % 2 == 1:
-        res = 0
-    else:
-        res = res + c - i
-res += c
-*/
