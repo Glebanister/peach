19b15d7375a9f53c753ccf282fb2b7159e206bc5
Glebanister <glebmar2001@gmail.com>
2020-09-05 00:34:51 +0300

    Remove filename from Exception

--- include/Exception.hpp
+++ include/Exception.hpp
@@ -38,38 +38,32 @@ private:
 class PositionalException : PeachException
 {
 public:
-    PositionalException(const std::string &filename,
-                        std::size_t line,
+    PositionalException(std::size_t line,
                         std::size_t position,
                         const std::string &exceptionClass,
                         const std::string &comment)
         : PeachException(exceptionClass,
-                         comment + " in " + filename +
-                             ":" + std::to_string(line) +
+                         comment + " in " +
+                             std::to_string(line) +
                              ":" + std::to_string(position)),
           line_(line),
-          position_(position),
-          filename_(filename)
+          position_(position)
     {
     }
 
     std::size_t getLine() const noexcept { return line_; }
     std::size_t getPosition() const noexcept { return position_; }
-    std::string getFilename() const { return filename_; }
 
 private:
     const std::size_t line_, position_;
-    const std::string filename_;
 };
 
 class IndentationException : public PositionalException
 {
 public:
-    IndentationException(const std::string &filename,
-                         std::size_t line,
+    IndentationException(std::size_t line,
                          std::size_t position)
-        : PositionalException(filename,
-                              line,
+        : PositionalException(line,
                               position,
                               "IndentationException", "bad indentation")
     {
