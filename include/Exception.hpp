3bff959579a53572cbc2e60ce3584532dc38721b
Glebanister <glebmar2001@gmail.com>
2020-09-05 18:01:38 +0300

    Add syntax error and comfortable throwing

--- include/Exception.hpp
+++ include/Exception.hpp
@@ -2,6 +2,8 @@
 
 #include <stdexcept>
 
+#include "Token.hpp"
+
 namespace peach
 {
 namespace exception
@@ -24,13 +26,13 @@ private:
     const std::string report_;
 };
 
-class PositionalException : public PeachException
+class PositionalError : public PeachException
 {
 public:
-    PositionalException(std::size_t line,
-                        std::size_t position,
-                        const std::string &exceptionClass,
-                        const std::string &comment)
+    PositionalError(std::size_t line,
+                    std::size_t position,
+                    const std::string &exceptionClass,
+                    const std::string &comment)
         : PeachException(exceptionClass,
                          comment + " in " +
                              std::to_string(line) +
@@ -47,14 +49,59 @@ private:
     const std::size_t line_, position_;
 };
 
-class IndentationException : public PositionalException
+template <typename ExceptionT,
+          typename = decltype(std::declval<ExceptionT>().getLine()),
+          typename = decltype(std::declval<ExceptionT>().getPosition())>
+inline void throwFromTokenIterator(const std::vector<token::TokenPtr>::iterator &it)
+{
+    throw ExceptionT((*it)->getLine(),
+                     (*it)->getPosition());
+}
+
+class IndentationError : public PositionalError
 {
 public:
-    IndentationException(std::size_t line,
-                         std::size_t position)
-        : PositionalException(line,
-                              position,
-                              "IndentationException", "bad indentation")
+    IndentationError(std::size_t line,
+                     std::size_t position)
+        : PositionalError(line,
+                          position,
+                          "IndentationError", "bad indentation")
+    {
+    }
+};
+
+class SyntaxError : public PositionalError
+{
+public:
+    SyntaxError(std::size_t line,
+                std::size_t position)
+        : PositionalError(line,
+                          position,
+                          "SyntaxError", "invalid syntax")
+    {
+    }
+};
+
+class UnexpectedElseError : public PositionalError
+{
+public:
+    UnexpectedElseError(std::size_t line,
+                        std::size_t position)
+        : PositionalError(line,
+                          position,
+                          "UnexpectedElseError", "can not process 'else' if it not preceded by 'if'")
+    {
+    }
+};
+
+class UndefinedOperatorError : public PositionalError
+{
+public:
+    UndefinedOperatorError(std::size_t line,
+                           std::size_t position)
+        : PositionalError(line,
+                          position,
+                          "UndefinedOperatorError", "can't find operator")
     {
     }
 };
