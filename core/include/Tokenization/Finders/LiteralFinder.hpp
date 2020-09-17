5999ddf39538feeb9fb0f8235dc1afce92a61525
Glebanister <glebmar2001@gmail.com>
2020-09-17 17:32:27 +0300

    Add literal termination after endline

--- core/include/Tokenization/Finders/LiteralFinder.hpp
+++ core/include/Tokenization/Finders/LiteralFinder.hpp
@@ -10,7 +10,7 @@ namespace peach
 namespace fsm
 {
 // Finite state machine, finds string literals.
-
+// escaped characters: [escaped, real]
 template <char Separator>
 class LiteralFinder : public FiniteStateMachine
 {
@@ -18,6 +18,7 @@ public:
     LiteralFinder(token::tokenCategory_t category)
     {
         auto node = getRoot()->template addTransitionToNewNode<transition::SingleCharTransitionTemplate<Separator>>(token::tokenCategory::UNDEFINED);
+        node->template addTransitionToNewNode<transition::SingleCharTransitionTemplate<'\n'>>(token::tokenCategory::UNDEFINED);
         node->template addTransition<transition::TransitionNegation<transition::SingleCharTransitionTemplate<Separator>>>(node);
         auto terminal = node->template addTransitionToNewNode<transition::SingleCharTransitionTemplate<Separator>>(token::tokenCategory::UNDEFINED);
         terminal->template addTransitionToNewNode<transition::TrueTransition>(category);
