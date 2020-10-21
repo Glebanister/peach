86293b905c60ab83edbc966e6010d60163ea1375
Glebanister <glebmar2001@gmail.com>
2020-10-22 00:44:18 +0300

    Rework token

--- core/include/Tokenization/Token.hpp
+++ core/include/Tokenization/Token.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <ostream>
 #include <string>
 #include <utility>
 #include <vector>
@@ -9,202 +10,91 @@ namespace peach
 {
 namespace token
 {
-using tokenCategory_t = std::size_t;
-
-struct tokenCategory
+enum class tokenCategory
 {
-    tokenCategory() = delete;
-
-    static constexpr tokenCategory_t
-        UNDEFINED = 0,
-
-        // Token is a name,
-        // Starts with a latin letter,
-        // May contain:
-        //     - contains digits
-        //     - latin letters
-        //     - underscores
-        // Examples:
-        //     - 'VariableName1'
-        //     - 'variable_name_2'
-        NAME = 1,
-
-        // Token is a value - predefined, or user-defined,
-        // Examples:
-        //     - '123'                for std::int32_t
-        //     - '"this is a string"' for std::string
-        VALUE = 2,
-
-        // Token is an binary operator,
-        // Descending priority:
-        //     - '&'        logical and
-        //     - '|'        logical or
-        //     - '*'        multiplication
-        //     - '/'        division
-        //     - '%'        module
-        //     - '+'        addition
-        //     - '-'        substraction
-        OPERATOR_BI = 3,
-
-        // Token is an unary operator,
-        // Descending priority:
-        //     - '!'        logical negation
-        OPERATOR_UN = 4,
-
-        // Token states for assignment,
-        // Only name token may be on the left,
-        // Any expression could be on the right.
-        //     - '='  assign
-        //     - 'X=' X and assign
-        ASSIGNMENT = 5,
-
-        // Token is 'if' in conditional chain
-        COND_IF = 6,
-
-        // Token is 'elif' in conditional chain
-        COND_ELIF = 7,
-
-        // Token is 'else' in conditional chain
-        COND_ELSE = 8,
-
-        // Token is 'while' loop
-        LOOP_WHILE = 9,
-
-        // Token is a opening round bracket
-        BRACKET_OPEN = 10,
-
-        // Token is a closing round bracket
-        BRACKET_CLOSE = 11,
-
-        // Token is an endline character
-        SEP_ENDL = 12,
-
-        // Token is a tabulation character
-        SEP_TAB = 13,
-
-        // Token is a space character
-        SEP_SPACE = 14,
-
-        // Token is floating point number
-        // Example: 420.69
-        VALUE_FLOATING = 15,
-
-        // Token is integer
-        // Example: 239
-        VALUE_INT = 16,
-
-        // Token is colon ':'
-        COLON = 17,
-
-        // Token is semicolon ';'
-        SEMICOLON = 18,
-
-        // Token is variable declaration
-        DECLARATION = 19,
-
-        _TOKEN_TOTAL = 20;
+    UNDEFINED,
+    IDENTIFIER,
+    INTEGER,
+    OPERATOR,
+    BRACKET_OPEN,
+    BRACKET_CLOSE,
+    SEP,
+    COLON,
+    SEMICOLON,
+    _TOKEN_TOTAL
 };
 
-static std::vector<std::string> tokenCategoryString = {
-    "UNDEFINED",
-    "NAME",
-    "VALUE",
-    "OPERATOR_BI",
-    "OPERATOR_UN",
-    "ASSIGNMENT",
-    "COND_IF",
-    "COND_ELIF",
-    "COND_ELSE",
-    "LOOP_WHILE",
-    "BRACKET_OPEN",
-    "BRACKET_CLOSE",
-    "SEP_ENDL",
-    "SEP_TAB",
-    "SEP_SPACE",
-    "VALUE_FLOATING",
-    "VALUE_INT",
-    "COLON",
-    "SEMICOLON",
-    "DECLARATION",
-};
-
-inline void registerTokenCategoryString(const std::string &name)
+struct Token
 {
-    tokenCategoryString.push_back(name);
-}
-
-inline void printCategory(std::ostream &os, tokenCategory_t category)
-{
-    if (static_cast<std::size_t>(category) >= tokenCategoryString.size())
-    {
-        throw std::invalid_argument("can not print category, it is not registred yet");
-    }
-    os << tokenCategoryString[static_cast<std::size_t>(category)];
-}
-
-class Token
-{
-public:
-    Token(tokenCategory_t category,
-          std::string token,
-          std::size_t line,
-          std::size_t linePosition,
-          std::size_t textPosition)
-        : category_(std::move(category)),
-          token_(std::move(token)),
-          line_(std::move(line)),
-          linePosition_(std::move(linePosition)),
-          textPosition_(std::move(textPosition))
-    {
-    }
-
-    tokenCategory_t getCategory() const noexcept { return category_; }
-    std::string getTokenString() const { return token_; }
-    std::size_t getLine() const noexcept { return line_; }
-    int getLinePosition() const noexcept { return linePosition_; }
-    int getTextPosition() const noexcept { return textPosition_; }
-
-    void setCategory(tokenCategory_t category) noexcept { category_ = category; }
-
-private:
-    tokenCategory_t category_;
-    std::string token_;
-    std::size_t line_, linePosition_, textPosition_;
+    tokenCategory category;
+    std::string token;
+    std::size_t textPosition;
 };
 
 using TokenPtr = std::unique_ptr<Token>;
 
-inline constexpr bool isEndline(tokenCategory_t category) noexcept
+inline bool isEndline(const Token &token) noexcept
 {
-    return category == tokenCategory::SEP_ENDL;
-}
-
-inline constexpr bool isEndline(char c) noexcept
-{
-    return c == '\n';
+    return token.token == "\n";
 }
 
 inline bool isEndline(const TokenPtr &tk) noexcept
 {
-    return isEndline(tk->getCategory());
+    return isEndline(*tk);
 }
 
-inline constexpr bool isSeparator(tokenCategory_t category) noexcept
+inline bool isSeparator(const Token &token) noexcept
 {
-    return category == tokenCategory::SEP_ENDL ||
-           category == tokenCategory::SEP_SPACE ||
-           category == tokenCategory::SEP_TAB;
+    return token.category == tokenCategory::SEP;
 }
 
 inline bool isSeparator(const TokenPtr &tk) noexcept
 {
-    return isSeparator(tk->getCategory());
+    return isSeparator(*tk);
 }
 
-inline std::size_t getTokenOperatorArity(tokenCategory_t category)
+std::ostream &operator<<(std::ostream &os, tokenCategory category)
 {
-    return category == tokenCategory::OPERATOR_UN ? 1 : 2;
+    std::string stringView;
+    switch (category)
+    {
+    case tokenCategory::UNDEFINED:
+        stringView = "undefined";
+        break;
+    case tokenCategory::IDENTIFIER:
+        stringView = "identifier";
+        break;
+    case tokenCategory::INTEGER:
+        stringView = "integer";
+        break;
+    case tokenCategory::OPERATOR:
+        stringView = "operator";
+        break;
+    case tokenCategory::BRACKET_OPEN:
+        stringView = "opening bracket";
+        break;
+    case tokenCategory::BRACKET_CLOSE:
+        stringView = "closing bracket";
+        break;
+    case tokenCategory::SEP:
+        stringView = "separator";
+        break;
+    case tokenCategory::COLON:
+        stringView = "colon";
+        break;
+    case tokenCategory::SEMICOLON:
+        stringView = "semicolon";
+        break;
+    default:
+        stringView = "unrecognized token category";
+        break;
+    }
+    os << "[" << stringView << "]";
 }
 
+std::ostream &operator<<(std::ostream &os, const Token &token)
+{
+    return os << token.category << " '" << token.token << "' at " << token.textPosition;
+}
 } // namespace token
 } // namespace peach
